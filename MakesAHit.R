63c04c80136e7de98f2ed753a108bfc1c610698c
Valeri Kozarev <vikozare@ncsu.edu>
2019-02-27 19:19:04 -0800

    added a plot for PCA, working on dim. reduction

--- MakesAHit.R
+++ MakesAHit.R
@@ -1,4 +1,5 @@
 # Group 05 - Spotify Hit Prediction Project
+library(ggbiplot)
 
 # Step 1: Read in data and perform PCA
 spotify_rawdata <- read.csv("top2018.csv")
@@ -7,3 +8,4 @@ spotify_pca <- prcomp(spotify_rawdata[,c(4:15)], center=TRUE, scale=TRUE)
 summary(spotify_pca)
 
 # PCA summary seems to indicate PC1-6 are important
+ggbiplot(spotify_pca)
