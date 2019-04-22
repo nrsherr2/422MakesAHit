cccc77ae7d8ea3c1899beee931a0d14df931e19b
Nikhil Kaul <nkaul2@ncsu.edu>
2019-04-22 11:40:48 -0400

    added hyperparamter tuning

--- MakesHit_nkaul2.R
+++ MakesHit_nkaul2.R
@@ -7,6 +7,7 @@ set.seed(123)
 setwd("/Users/nikhilkaul/Documents/Spring Semester 2019/CSC 422/Final Project")
 mydata = read.csv(file = "spotify_training_data.csv")
 my_data <- mydata[, c(2,3,4,5,6,7,8,9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19)]
+
 p = ggplot(my_data,aes(x=danceability,  
                     y=energy, 
                     color=genre))
@@ -19,15 +20,24 @@ sample <- sample.int(n = nrow(my_data), size = floor(.66*nrow(my_data)), replace
 train <- my_data[sample, ]
 test  <- my_data[-sample, ]
 
+x <- train[,4:17]
+y <- train[,18]
+bestmtry <- tuneRF(x, y, stepFactor=1.5, improve=1e-5, ntree=300)
 
-rf = randomForest(genre ~ energy + danceability + speechiness + valence + loudness + liveness + instrumentalness
-                    + acousticness,  ntree = 100,data = train)
-plot(rf)
+rf = randomForest(genre ~ energy + danceability + speechiness + valence + liveness + instrumentalness
+                    + acousticness + loudness + popularity + tempo + mode + explicit + key,  ntree = 300,data = train, mtry=3)
+plot(rf, main = "Random Forest Algorithm")
+legend("topright", 
+       legend = c("OOB", "country", "edm", "metal", "pop", "rap", "rock"),
+       fill = c("black", "red", "green", "blue", "cyan", "pink", "black"), cex = .70
+)
 print(rf)
 
 train$predicted.response = predict(rf , train)
-##print(confusionMatrix(data = train$predicted.response, reference = train$genre, ))
+print(confusionMatrix(data = train$predicted.response, reference = train$genre))
 
 test$predicted.response = predict(rf , test)
-##print(confusionMatrix(data = test$predicted.response, reference = test$genre, ))
+print(confusionMatrix(data = test$predicted.response, reference = test$genre))
+
+
 
